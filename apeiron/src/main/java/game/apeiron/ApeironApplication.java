e25b7c5dee4630d01b8e12c6db345aa849b988e0
voin947 <andrewpoberezhnik@gmail.com>
2020-03-26 13:57:51 +0100

    initial

---
+++ apeiron/src/main/java/game/apeiron/ApeironApplication.java
@@ -0,0 +1,13 @@
+package game.apeiron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApeironApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApeironApplication.class, args);
+	}
+
+}
---
+++ apeiron/src/test/java/game/apeiron/ApeironApplicationTests.java
@@ -0,0 +1,13 @@
+package game.apeiron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApeironApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
